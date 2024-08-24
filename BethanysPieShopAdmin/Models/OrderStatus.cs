a13b89de34fdeed0096e0b1910a226f1f53ac334
Maciej Broda <maciejbroda01@gmail.com>
2024-08-24 23:16:38 +0200

    Creating the Model

---
+++ BethanysPieShopAdmin/Models/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace BethanysPieShopAdmin.Models
+{
+    public enum OrderStatus
+    {
+        Received,
+        Paid,
+        Processing,
+        OutForDelivery,
+        Delivered
+    }
+
+}
