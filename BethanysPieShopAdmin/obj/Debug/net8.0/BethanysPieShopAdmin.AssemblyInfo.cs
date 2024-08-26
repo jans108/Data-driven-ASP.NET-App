9d021ee4e552a11daca4af38f3d4479ad4eee67a
Maciej Broda <maciejbroda01@gmail.com>
2024-08-26 22:22:53 +0200

    Adding the Category Overview

--- BethanysPieShopAdmin/obj/Debug/net8.0/BethanysPieShopAdmin.AssemblyInfo.cs
+++ BethanysPieShopAdmin/obj/Debug/net8.0/BethanysPieShopAdmin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShopAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5aa25b72857f824e82a0e096ab61c2be48f0fb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda2903781c0d067b9e4e7f154192c1023ca00bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShopAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShopAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
